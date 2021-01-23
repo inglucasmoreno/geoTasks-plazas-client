9a4f16e69666a44bfef7c921f65371ffe1ef35fb
Lucas Omar Moreno <ing.lucasmoreno@gmail.com>
2021-01-23 17:03:42 -0300

    Texto flotante para iconos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  base_url: 'https://geotasks-plazas.herokuapp.com/api'
+  // base_url: 'https://geotasks-plazas.herokuapp.com/api'
+  base_url: 'http://190.107.118.137:3000/api'
 };
